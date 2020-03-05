61d34f660c879cbccf9e3940ccc7017fef74417e
jacobriesen <jmriesen@iastate.edu>
2020-03-05 16:41:48 -0600

    initial commit.

---
+++ main/java/events/remote/UpdateTextRemote.java
@@ -0,0 +1,16 @@
+package events.remote;
+
+import coms362.cards.streams.Marshalls;
+import model.Player;
+
+public class UpdateTextRemote implements Marshalls {
+
+	public UpdateTextRemote(Player p, int i) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String marshall() {
+		return "";
+	}
+
+}
