c6920f71ce29b37f65b56fd35cad675e5fe706b2
Matthew Tan Creti <tancreti@iastate.edu>
2020-03-23 12:29:29 -0500

    Added createButton() to cards362 for creating generic buttons and removed hard coded deal button.

--- src/main/java/events/remote/CreateButtonRemote.java
+++ src/main/java/events/remote/CreateButtonRemote.java
@@ -4,33 +4,25 @@ import coms362.cards.streams.Marshalls;
 import model.Location;
 
 public class CreateButtonRemote implements Marshalls {
-	
-	private String id;
-	private String label;
-	private Location loc;
-	private String evtName;
-	
-	public CreateButtonRemote(String id, String label, Location p, String evtName){
-		this.id = id;
-		this.label = label;
-		loc = p; 
-		this.evtName = evtName;
-	}
+    private String id;
+    private String evtName;
+    private String label;
+    private Location loc;
 
-	public String marshall() {
-		return String.format(""
-			+ "var btn=$('<button>%s</button>')"
-			+ ".prop('id', '%s')"
-			+ ".click(function(){"
-			+ "  doSend(JSON.stringify({event: '%s'}));\n"
-			+ "})\n;"
-			+ "$('#card-table').append(btn);\n",			
-			label, id, evtName
-		);
-	}
+    public CreateButtonRemote(String id, String evtName, String label, Location p) {
+        this.id = id;
+        this.evtName = evtName;
+        this.label = label;
+        loc = p;
+    }
 
-	public String stringify() {
-		return "CreateButton "+label;
-	}
+    public String marshall() {
+        return String.format("cards362.createButton('%s', '%s', '%s', %d, %d);\n",
+                id, evtName, label, loc.getX(), loc.getY());
+    }
+
+    public String stringify() {
+        return "CreateButton " + label;
+    }
 
 }
