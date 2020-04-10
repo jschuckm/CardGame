958c189016750ea4c9ab345d0f6f334ffd0095af
Jared Schuckman <jschuckm@iastate.edu>
2020-04-10 15:52:03 -0500

    Cleaned up comments and added Java Doc author comment for YuhaoTest and DakshTest

--- src/test/java/coms362/cards/DakshTest.java
+++ src/test/java/coms362/cards/DakshTest.java
@@ -8,6 +8,8 @@ import java.util.zip.CRC32;
 
 import coms362.cards.fiftytwo.*;
 import model.Card;
+import model.Location;
+
 import org.junit.Test;
 
 import coms362.cards.abstractcomp.Player;
@@ -21,62 +23,21 @@ import events.inbound.CardEvent;
 import events.inbound.EndPlay;
 import model.TableBase;
 import model.Pile;
-
+/**
+ * 
+ * @author Daksh Goel
+ *
+ */
 public class DakshTest {
 
-    static final long expectedSig = 1427999153L;
-
     @Test
-    public void test() {/*
-        //set up game and match resources to provision play loop
-        InBoundQueue inQ = new InBoundQueue();
-        //pre-load the input stream with the input for this test
-        inQ.add(new DealEvent());
-        inQ.add(new CardEvent("2"));
-        inQ.add(new CardEvent("3"));
-        inQ.add(new CardEvent("4"));
-        inQ.add(new EndPlay()); //artifice to stop the test
-
-        List<View> views = new ArrayList<View>();
-        //we keep a reference to the concrete type for later
-        LoggingView  p1View = new LoggingView();
-        views.add(p1View);
-
-        Player player = new PickupPlayer(1); //ditto for players
-
-        // initialize the local model for Pu52 match
-        Table table = new TableBase();
-        table.apply(new PickupInitCmd());
-        Rules rules = new PickupRules();
-
-        PlayController mainloop = new PlayController(inQ, rules);
-        mainloop.play(table, player, views);
-
-
-        CRC32 sig = new CRC32();
-        String log = p1View.getLog();
-        System.out.println(log);
-        sig.update(log.getBytes());
-        long sValue = sig.getValue();
-        System.out.println(sValue);
-
-        assertEquals(expectedSig, sValue);
-        */
-    }
-    @Test
-    public void checkScore(){
-        /*
-        Table table = new TableBase();
-        table.apply(new PickupInitCmd());
-        Player p = new PickupPlayer(1);
-        Pile discard = table.getPile("discardPile");
-        Card c = discard.getCard("" + 0);
-        //PickupMove pickup = new PickupMove(c, p);
-        table.apply(new PickupMove(c, p));
-        assertEquals(p.addToScore(0), 0);
-        */
-
-
+    public void checkCardFromPile(){
+        Pile newPile = new Pile("Test Pile", new Location(0, 0));
+        Card c = new Card();
+        c.setId(4);
+        newPile.addCard(c);
+        Card myCard = newPile.getCard("4");
+        assertTrue(4 == myCard.getId());
     }
 
 }
